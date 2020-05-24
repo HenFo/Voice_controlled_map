4d0840b940af22b677871c161e628d8f81b30b35
Henry Fock <h_fock01@wwu.de>
2020-05-24 19:19:21 +0200

    close animation

--- actions/js/basic.js
+++ actions/js/basic.js
@@ -1,37 +1,3 @@
-var images = [
-    "images\\david-clode-e3irr6H7e5s-unsplash.jpg",
-    "images\\simon-berger-qOmxP7W8svM-unsplash.jpg"
-];
-
-function next() {
-    let val = document.getElementById("value");
-    let image = document.getElementById("image");
-    let number = parseInt(val.innerHTML);
-    val.innerHTML = ++number;
-    if (number < images.length && number >= 0) {
-        image.src = images[number];
-        image.style.visibility = "visible";
-    }
-    else {
-        image.style.visibility = "hidden";
-    }
-}
-
-function prev() {
-    let val = document.getElementById("value");
-    let image = document.getElementById("image");
-    let number = parseInt(val.innerHTML);
-    val.innerHTML = --number;
-    if (number < images.length && number >= 0) {
-        image.src = images[number];
-        image.style.visibility = "visible";
-    }
-    else {
-        image.style.visibility = "hidden";
-    }
-}
-
-
 /**
  * sets js to sleep
  * function that uses sleep must be declared async
@@ -43,7 +9,7 @@ function sleep(ms) {
 }
 
 async function doStuff() {
-    aniNext();
+    aniClose();
 }
 async function doStuff2() {
     aniPrev();
@@ -70,4 +36,19 @@ async function aniPrev() {
     button.style.backgroundColor = "";
     button.style.boxShadow = "";
     prev();
+}
+
+async function aniClose() {
+    let button = document.getElementById("closeIcon");
+    let card = document.getElementById("close")
+
+    await sleep(300);
+    button.style.textShadow = "0 0 10px rgb(70, 144, 255)";
+    button.style.color = "rgb(170,0,0)";
+    await sleep(1000);
+    closeCard();
+    await sleep(5000)
+    openCard();
+    button.style.textShadow = "";
+    button.style.color = "";
 }
